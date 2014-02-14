67de3f3df0ce3b86b8ce0bce984ea534edc5cbc1
Lloyd Henning <lloyd.w.henning@gmail.com>
2014-02-14 10:33:20 +0000

    Updated git submodule and use 0.6.5+ package style

--- package.js
+++ package.js
@@ -1,9 +1,10 @@
 Package.describe({
-  summary: "tween.js repackaged for Meteor"
+  summary: "Super simple, fast and easy to use tweening engine which incorporates optimised Robert Penner's equations."
 });
 
 Package.on_use(function (api, where) {
   where = where || ['client', 'server'];
-  
+
   api.add_files('lib/tween.js/src/Tween.js', where);
+  api.add_files('export-tween.js', where);
 })
