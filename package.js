df59aedce930ce98b3d0db5a2ae44fbb6e4279e1
Brent Anderson <me@brentjanderson.com>
2014-08-21 23:24:39 -0600

    Bump to r14 and add 0.9.0 package compatibility

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: "tween.js repackaged for Meteor"
+  summary: "tween.js repackaged for Meteor",
+  version: "0.14.0",
+  git: "https://github.com/vedosity/meteor-tween.git"
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
   where = where || ['client', 'server'];
  
   api.add_files('lib/Tween.js', where);
